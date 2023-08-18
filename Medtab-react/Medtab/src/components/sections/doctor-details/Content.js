60edd87f61229fc436344a1101ae04fa3d3b9d65
Matias <matiaslamonico1c@gmail.com>
2023-08-18 02:15:06 -0300

    cambios doctor detail

--- Medtab-react/Medtab/src/components/sections/doctor-details/Content.js
+++ Medtab-react/Medtab/src/components/sections/doctor-details/Content.js
@@ -65,7 +65,7 @@ class Content extends Component {
                                             </div>
                                             <div className="col-md-4">
                                                 <div className="menu nav-item border-0">
-                                                    <Link to="#" className="nav-link p-0" onClick={() => document.getElementById('reviews').scrollIntoView({ behavior: 'smooth' })}></Link>
+                                                    <Link to="#" className="nav-link p-0" onClick={() => document.getElementById('reviews').scrollIntoView({ behavior: 'smooth' })}>Reseñas</Link>
                                                 </div>
                                             </div>
                                         </div>
@@ -77,14 +77,14 @@ class Content extends Component {
                                     <div className="spacer">
                                     </div>
                                     <div id="contact">
-                                        <h4>{item.name} Ubicacion &amp; Informacion de Contacto</h4>
+                                        <h4>{item.name} ubicación e información de contacto</h4>
                                         <div className="sigma_contact-wrapper">
                                             <div className="sigma_contact-map">
                                                 <iframe title={item.name} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9914406081493!2d2.292292615201654!3d48.85837360866272!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2964e34e2d%3A0x8ddca9ee380ef7e0!2sEiffel%20Tower!5e0!3m2!1sen!2sin!4v1571115084828!5m2!1sen!2sin" height={600} allowFullScreen>
                                                 </iframe>
                                             </div>
                                             <div className="sigma_contact-blocks">
-                                                <h5>Hospital Address</h5>
+                                                <h5>Direccion del hospital</h5>
                                                 <div className="row">
                                                     <div className="col-md-4">
                                                         <div className="sigma_contact-block style-3">
