e6167f9c0e24ab1642087053114f3ce609cb8289
Matias <matiaslamonico1c@gmail.com>
2023-08-18 19:00:59 -0300

    mapita

--- Medtab-react/Medtab/src/components/sections/doctor-details/Content.js
+++ Medtab-react/Medtab/src/components/sections/doctor-details/Content.js
@@ -80,7 +80,7 @@ class Content extends Component {
                                         <h4>{item.name} ubicación e información de contacto</h4>
                                         <div className="sigma_contact-wrapper">
                                             <div className="sigma_contact-map">
-                                                <iframe title={item.name} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9914406081493!2d2.292292615201654!3d48.85837360866272!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2964e34e2d%3A0x8ddca9ee380ef7e0!2sEiffel%20Tower!5e0!3m2!1sen!2sin!4v1571115084828!5m2!1sen!2sin" height={600} allowFullScreen>
+                                                <iframe title={item.name} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3403.975860563968!2d-64.20174002360993!3d-31.442331497507386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432a25925957c33%3A0xcbc974304cd4e547!2sHospital%20Privado%20Universitario%20de%20C%C3%B3rdoba!5e0!3m2!1ses-419!2sar!4v1692395865251!5m2!1ses-419!2sar" height={600} allowFullScreen>
                                                 </iframe>
                                             </div>
                                             <div className="sigma_contact-blocks">
